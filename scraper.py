d5ecbf9c7e3871ee73804387b47f1d26e62a68b5
Shalvin Shaji <shalvinpshaji@gmail.com>
2022-02-09 15:21:06 +0530

    Minor bugs fixed handled cases where videos are divided into seperate containers

--- scraper.py
+++ scraper.py
@@ -1,24 +1,65 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.keys import Keys
+
+
 YOUTUBE_TRENDING_URL: str = 'https://www.youtube.com/feed/trending'
-CATEGORIES = ['short-trending', 'trending', 'recently-trending']
 
-driver = webdriver.Edge()
-driver.get(YOUTUBE_TRENDING_URL)
-time.sleep(2)
-print("Title :", driver.title)
-contents = driver.find_element(By.ID, 'contents')
-video_container = contents.find_elements(By.ID, 'grid-container')
-scraped = {}
 
-for container, category in zip(video_container, CATEGORIES):
-    titles = container.find_elements(By.ID, 'title-wrapper')
-    data = []
-    for title in titles:
-        data.append(title.text)
-    scraped[category] = data
-print(scraped)
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Edge(options=options)
+    return driver
 
-driver.quit()
+
+def parse_video(video):
+    thumbnail = video.find_element(By.ID, 'img')
+    thumbnail_url = thumbnail.get_attribute('src')
+    video_title_tag = video.find_element(By.ID, 'video-title')
+    video_url = video_title_tag.get_attribute('href')
+    video_title = video_title_tag.text
+    spans = video.find_element(By.ID, 'metadata-line').find_elements(By.TAG_NAME, 'span')
+    video_views = spans[0].text
+    video_upload = spans[1].text
+    data = {
+        'title': video_title,
+        'video_url': video_url,
+        'thumbnail_url': thumbnail_url,
+        'views': video_views,
+        'upload_time': video_upload
+    }
+    return data
+
+
+def get_videos(driver):
+    scraped = {}
+    contents = driver.find_element(By.ID, 'contents')
+    video_containers = contents.find_elements(By.ID, 'grid-container')
+
+    for i, video_container in enumerate(video_containers):
+        videos = video_container.find_elements(By.TAG_NAME, 'ytd-video-renderer')
+        datas = []
+        for video in videos:
+            video.location_once_scrolled_into_view
+            data = parse_video(video)
+            print(data)
+            datas.append(data)
+        scraped[f"container{i}"] = datas
+        print(len(datas), end='\n\n')
+    return scraped
+
+
+if __name__ == "__main__":
+    edge_driver = get_driver()
+    edge_driver.get(YOUTUBE_TRENDING_URL)
+    time.sleep(3)
+    html = edge_driver.find_element(By.TAG_NAME, 'html')
+    print("Title :", edge_driver.title)
+    content = get_videos(edge_driver)
+
+    edge_driver.close()
+    edge_driver.quit()
